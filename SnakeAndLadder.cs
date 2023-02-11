8fbc23eeb1571be91ac0969beed7910e58da0c3d
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-11 14:22:12 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake.

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadder
+    {
+        public static void Game()
+        {
+            int position = 0;
+
+            Console.WriteLine("\nWelcome Player");
+            Console.WriteLine("Player at position : " + position);
+
+
+            int dice;
+
+            Random random = new Random();
+            dice = random.Next(1, 7);
+
+            Console.WriteLine("\nPlayer rolls dice :" + dice);
+
+            int option;
+
+            option = random.Next(0, 3);
+
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:
+                    Console.WriteLine("Ladder");
+                    position += dice;
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    position -= dice;
+                    break;
+            }
+            Console.WriteLine("Player Current Position : " + position);
+
+
+        }
+    }
+}

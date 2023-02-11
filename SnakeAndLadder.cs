1b3a05671c0c306682bb4a487ed3d48b159a95e0
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-11 14:02:31 +0530

    The Player rolls the die to get a number between 1 to 6.

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadder
+    {
+        public static void Game()
+        {
+            int position = 0;
+
+            Console.WriteLine("\nWelcome Player");
+            Console.WriteLine("Player at position : " + position);
+
+
+            int dice;
+
+            Random random = new Random();
+            dice = random.Next(1, 7);
+
+            Console.WriteLine("\nPlayer rolls dice :" + dice);
+
+
+        }
+    }
+}

4bad4bf747e3bf1e3d873c4ec4a79e71f5da07ee
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-11 13:42:39 +0530

    Single Player At Start Position

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadder
+    {
+        public static void Game()
+        {
+            int position = 0;
+
+            Console.WriteLine("\nWelcome Player1");
+            Console.WriteLine("Player1 at position : " + position);
+        }
+    }
+}
